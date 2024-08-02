158be6f64c2e96d51c54af66113a2d6761f96620
Dayvid Silva <dayvid@navitdigital.com.br>
2024-08-01 23:33:08 -0300

    realtime version

--- server.js
+++ server.js
@@ -203,6 +203,6 @@ io.on("connection", (socket) => {
 
 const PORT = process.env.PORT || 4000;
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
